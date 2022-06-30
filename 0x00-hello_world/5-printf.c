4fbc4c18cc530d5f56d0576bc792c50df40c97f3
briviaallan <briviallan@gmail.com>
2022-06-30 14:39:40 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 /**
  * main - to print the output
- * return: 0 shows success non zero shows error
+ * return: shows success non zero shows error
  */
 
 
 int main(void)
 {
-
-
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
-
-
 }
