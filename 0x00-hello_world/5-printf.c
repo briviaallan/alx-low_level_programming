84169a4ade260e891842ec4dbe03b835884440e0
briviaallan <briviallan@gmail.com>
2022-06-30 14:18:52 -0700

    corrections just done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+/**
+ * main - to print the output
+ * return: 0 shows success non zero shows error
+ */
+
 
 int main(void)
-
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return(0);
-}
 
-/*it writes out what is in red*/
+
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+
+
+}
