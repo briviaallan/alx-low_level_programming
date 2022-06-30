68916778a7e6fd2e7ae7ae73c582dd802ecbf6c9
briviaallan <briviallan@gmail.com>
2022-06-30 14:29:30 -0700

    mit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+/**
+ * main - to print output
+ * return: 0 shows success non zeroshows error
+ */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
 /* this is supposed to pront out the string in red */
