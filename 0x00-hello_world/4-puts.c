ee200b4f75ab70c4b131ed90414db189c879b208
briviaallan <briviallan@gmail.com>
2022-06-30 14:25:33 -0700

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,4 +5,4 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-/*this is supposed to pront out the string in red*/
+/* this is supposed to pront out the string in red */
