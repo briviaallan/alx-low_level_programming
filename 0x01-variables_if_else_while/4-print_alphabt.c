815e3a39cdd0099b740a7e50216f275e462e5b01
briviaallan <briviallan@gmail.com>
2022-07-04 00:18:46 +0300

    Brivia

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the loop
+ * Return: 0 shows success
+ */
+
+int main(void)
+{
+
+	int k;
+
+	for (k = 'a'; k <= 'z'; k++)
+	{
+	if (k != 'e' && k != 'q')
+		putchar(k);
+}
+	putchar(10);
+	return (0);
+
+}
