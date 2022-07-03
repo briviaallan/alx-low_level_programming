c025e84aa1bddef8e47862f376451a65d5241074
briviaallan <briviallan@gmail.com>
2022-07-04 01:31:35 +0300

    Brivia

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,10 +11,10 @@ int main(void)
 	int k;
 
 	for (k = 'a'; k <= 'z'; k++)
-	{
-	if (k != 'e' && k != 'q')
-		putchar(k);
-    }
+    {
+    if (k != 'e' && k != 'q')
+            putchar(k)
+}
 	putchar(10);
 	return (0);
 
